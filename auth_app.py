a9f9e65e7b18b0cf69b748019d1453b6a716b5bf
sudippodder <podder.sudip@gmail.com>
2026-03-19 15:52:53 +0530

    version change

--- auth_app.py
+++ auth_app.py
@@ -902,7 +902,7 @@ def main():
     # Now includes checking for persistent session file
     initialize_session_state()
     with st.sidebar:
-        st.header("App Navigation v3.2")
+        st.header("App Navigation v3.3")
         if "logged_in" in st.session_state and st.session_state["logged_in"] == True:
             if "params" not in st.session_state:
                 st.session_state.params = {}
@@ -920,7 +920,7 @@ def main():
             # Added 'Posts' to the list of pages
             if user['role'] == 'admin':
                 #user_pages = ['Dashboard', 'Profile', 'Tone','Content' ,'Humanize','Template','Generate Content','Template Contents','DB','Template Type','Content Generator','Content List']
-                user_pages = ['Dashboard', 'Profile','Template Type','Admin Content Generator','Content List','DB','Update Template Type']  #,'DB','Update Template Type'
+                user_pages = ['Dashboard', 'Profile','Template Type','Admin Content Generator','Content List']  #,'DB','Update Template Type'
             else:
                 #user_pages = ['Dashboard', 'Profile', 'Tone','Content','Content Generator','Content List']
                 user_pages = ['Dashboard', 'Profile', 'Content Generator','Content List']
